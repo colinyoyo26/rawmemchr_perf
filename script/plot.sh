c6c2fbf31ad084b1b7a74ea203f6aea2dea254aa
Yo Yi <colinyoyo26@gmail.com>
2019-12-19 19:25:29 +0800

    Cal avg CPE of from bench result

--- script/plot.sh
+++ script/plot.sh
@@ -1,8 +1,24 @@
+FILE=data.txt
+
+
 
 for i in `seq 10 1000 100000`
 do
     printf "$i "
     ./bench $i 
-done >> data.txt
+done >> $FILE
 
-gnuplot script/plotscript.gp && rm *.txt
+
+while IFS=" " read -r d1 d2 d3 d4 d5
+do
+    num=`expr $num + 1`
+    slowCPE=`expr $slowCPE + $d4`
+    fastCPE=`expr $fastCPE + $d5`
+done < $FILE
+
+slowCPE=`expr $slowCPE / $num`
+fastCPE=`expr $fastCPE / $num`
+
+gnuplot -e "fast = 'rawmemchr (CPE = $fastCPE)'; \
+            slow = 'orig (CPE = $slowCPE)'"      \
+            ./script/plotscript.gp && rm $FILE
